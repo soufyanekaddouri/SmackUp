a2592983706f2ebb36ae15fee0706503c0d4c33d
soufyanekaddouri <saffgoodman@gmail.com>
2016-03-20 13:40:21 +0100

    V0.3 (IMPORTANT CODE! READ GOOD!)

--- app/src/main/java/com/doppler/soufyane/smackup/MainActivity.java
+++ app/src/main/java/com/doppler/soufyane/smackup/MainActivity.java
@@ -1,8 +1,11 @@
 package com.doppler.soufyane.smackup;
 
+import android.app.Activity;
 import android.app.VoiceInteractor;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
@@ -31,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
     // CONTACTS LIST
     ListView contactsList;
     ListAdapter contactsAdapter;
-
+    // CONTACT INFO
+    String contactName;
+    int      contactNumber;
+    static final int PICK_CONTACT=1;
 
 
 
@@ -46,12 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
         OptionsIntent = new Intent(this, OptionsActivity.class);
 
-        pickContact    = new Intent(Intent.ACTION_PICK);
+        pickContact    = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
         pickContact.setType(ContactsContract.Contacts.CONTENT_TYPE);
 
         sendSMS = SmsManager.getDefault();
          // AN ARRAY OF CONTACTS
-        String[] contactsArray = {"Soufyane", "Mootje", "asaskd"};
+        String[] contactsArray = {};
 
         contactsList = (ListView) findViewById(R.id.contactsView);
         contactsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsArray);
@@ -62,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
             String pickedContact = String.valueOf(contactsList.getItemAtPosition(position));
             Toast.makeText(MainActivity.this, pickedContact, Toast.LENGTH_LONG).show();
+
             }
         });
     }
@@ -69,16 +76,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     // ADD A CONTACT (Material Design Button)
     public void addContactListener(View v) {
-        startActivity(pickContact);
+        startActivityForResult(pickContact, PICK_CONTACT);
+
     }
-
-
-
-
-
-
 
     // MENU AND OPTIONS
     @Override
@@ -95,4 +98,42 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_about) { Toast.makeText(this, "Developer: Soufyane Kaddouri", Toast.LENGTH_LONG).show(); }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        switch (reqCode) {
+            case (PICK_CONTACT) :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c =  getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+
+                        String id =c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                        String hasPhone =c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                        if (hasPhone.equalsIgnoreCase("1")) {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,
+                                    null, null);
+                            phones.moveToFirst();
+                            contactName = phones.getString(phones.getColumnIndex("data1"));
+                            System.out.println("number is:"+contactName);
+                        }
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+
+                    }
+                }
+                break;
+        }
+    }
+
+
+
+
 }
